5b41715ee905f48c78d0e1663d2bc8f0cafe27fd
João Vitor <vitor.alves99@yahoo.com>
2021-05-03 15:40:55 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/jo%C3%A3o-vitor-alves-da-silva-2b83551a3/" target="_blank" rel="noreferrer">João Vitor Alves da Silva</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
